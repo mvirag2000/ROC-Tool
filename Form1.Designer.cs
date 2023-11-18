5667ece45dcc36dc6a2c20100dea32214149537e
Mark Virag <mark@viragconsulting.com>
2023-11-17 18:52:47 -0600

    Add ROC tool to repo

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ROC_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(475, 381);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(197, 45);
+            this.textBox1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 75);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(457, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(59, 381);
+            this.trackBar1.Maximum = 99;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(410, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TrackBar trackBar1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ROC_Tool
+{
+    public partial class Form1 : Form
+    {
+        public DataTable resultsTable = new DataTable();
+        public DataTable confusionTable = new DataTable();
+        public Series series1 = new Series();
+        public Series series2 = new Series();
+        public Series series3 = new Series();
+        public Series series4 = new Series();
+        public decimal threshold = 0.50M; //threshold as <double> created some precision problems 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            confusionTable.Columns.Add("Cutoff", typeof(decimal));
+            confusionTable.Columns.Add("TPR", typeof(double));
+            confusionTable.Columns.Add("TNR", typeof(double));
+            confusionTable.Columns.Add("FPR", typeof(double));
+            confusionTable.Columns.Add("FNR", typeof(double));
+
+            series1 = chart1.Series.Add("TPR");
+            series1.ChartType = SeriesChartType.Line;
+            series1.Color = Color.DarkBlue;
+            series1.BorderWidth = 2;
+
+            series2 = chart1.Series.Add("TNR");
+            series2.ChartType = SeriesChartType.Line;
+            series2.Color = Color.DarkOrange;
+            series2.BorderWidth = 2;
+
+            series3 = chart1.Series.Add("Threshold");
+            series3.ChartType = SeriesChartType.Line;
+            series3.Color = Color.Red;
+            series3.BorderWidth = 1;
+
+            series4 = chart1.Series.Add("Dots");
+            series4.ChartType = SeriesChartType.Point;
+            series4.Color = Color.Black;
+            series4.BorderWidth = 3;
+
+            chart1.ChartAreas[0].AxisX.Title = "Threshold Value";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 1;
+
+            chart1.ChartAreas[0].AxisY.Title = "True Pos/Neg Rate";
+            chart1.ChartAreas[0].AxisY.Minimum = -0.05;
+            chart1.ChartAreas[0].AxisY.Maximum = 1.05;
+
+            chart1.Legends[0].Enabled = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                resultsTable.Columns.Add("Proba", typeof(double));
+                resultsTable.Columns.Add("Actual", typeof(int));
+                try
+                {
+                    using (StreamReader reader = new StreamReader(openFileDialog1.FileName))
+                    {
+                        bool isFirstRow = true;
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+
+                            if (isFirstRow)
+                            {
+                                //Think about creating headers from the file
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                resultsTable.Rows.Add(values);
+                            }
+                        }
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+
+                textBox1.Text = "Count: " + resultsTable.Rows.Count + "\r\n";
+                int totalPos = resultsTable.AsEnumerable().Where(row => row.Field<int>("Actual") == 1).Count();
+                textBox1.Text += "Positive: " + totalPos.ToString() + "\r\n";
+                int totalNeg = resultsTable.AsEnumerable().Where(row => row.Field<int>("Actual") == 0).Count();
+                textBox1.Text += "Negative: " + totalNeg.ToString() + "\r\n";
+
+                for (double cutoff = 0.01; cutoff <= 0.99; cutoff += 0.01)
+                {
+                    var assertPos = resultsTable.AsEnumerable().Where(row => row.Field<double>("Proba") >= cutoff);  //We need this subtable NOT just its count 
+                    int truePos = assertPos.AsEnumerable().Where(row => row.Field<int>("Actual") == 1).Count();
+                    int falsePos = assertPos.Count() - truePos;
+
+                    var assertNeg = resultsTable.AsEnumerable().Where(row => row.Field<double>("Proba") < cutoff);  //We need this subtable NOT just its count 
+                    int trueNeg = assertNeg.AsEnumerable().Where(row => row.Field<int>("Actual") == 0).Count();
+                    int falseNeg = assertNeg.Count() - trueNeg;
+
+                    double TPR = (totalPos > 0) ? (double)truePos / totalPos : 1;
+                    double TNR = (totalNeg > 0) ? (double)trueNeg / totalNeg : 1;
+                    double FPR = (totalNeg > 0) ? (double)falsePos / totalNeg : 1;
+                    double FNR = (totalPos > 0) ? (double)falseNeg / totalPos : 1;
+
+                    textBox1.Text += "TPR: " + TPR.ToString("F5") + " TNR: " + TNR.ToString("F5") + "\r\n";
+
+                    DataRow newRow = confusionTable.NewRow();
+                    newRow.ItemArray = new object[] { cutoff, TPR, TNR, FPR, FNR };
+                    confusionTable.Rows.Add(newRow);
+                }
+                foreach (DataRow row in confusionTable.Rows)
+                {
+                    series1.Points.AddXY(row["Cutoff"], row["TPR"]);
+                    series2.Points.AddXY(row["Cutoff"], row["TNR"]);
+                }
+                trackBar1_Scroll(sender, e);
+            }
+        }
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+        private void chart1_Click(object sender, EventArgs e)
+        {
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            threshold = (decimal) trackBar1.Value / 100;
+            textBox1.Text = threshold.ToString();
+            chart1.Series["Threshold"].Points.Clear();
+            for (double i = -0.05; i <= 1.05; i += 0.01)
+            {
+                series3.Points.AddXY(threshold, i);
+            }
+            var y1 = confusionTable.AsEnumerable().Where(row => row.Field<decimal>("Cutoff") == threshold);
+            chart1.Series["Dots"].Points.Clear(); 
+            foreach (DataRow row in y1)
+            {
+                series4.Points.AddXY(threshold, row["TPR"]);
+                series4.Points.AddXY(threshold, row["TNR"]);
+            }
+        }
+    }
+}
+
